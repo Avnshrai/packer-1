259d68a6e3cf7ee5ed3aba23baa4d75b4da445b7
rishi97 <rishabhaggarwal860@gmail.com>
2023-07-06 14:13:53 +0000

    added keycloak packer files

---
+++ keycloak/v21.1.1/rootfs/opt/coredge/scripts/keycloak/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+#set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/coredge/scripts/libcoredge.sh
+. /opt/coredge/scripts/liblog.sh
+. /opt/coredge/scripts/libkeycloak.sh
+
+# Load keycloak environment variables
+. /opt/coredge/scripts/keycloak-env.sh
+
+print_welcome_page
+
+if [[ "$*" = *"/opt/coredge/scripts/keycloak/run.sh"* ]]; then
+    info "** Starting keycloak setup **"
+    /opt/coredge/scripts/keycloak/setup.sh
+    info "** keycloak setup finished! **"
+fi
+
+echo ""
+exec "$@"
